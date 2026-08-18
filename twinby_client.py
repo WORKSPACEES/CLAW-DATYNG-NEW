4495dde36c7b4aac84bbda157a54ded45f65cfcf
WORKSPACE <f720576@gmail.com>
2026-08-18 10:03:53 +0300

    Update twinby_client.py

--- twinby_client.py
+++ twinby_client.py
@@ -31,10 +31,10 @@ def _proxy():
         }
     return {
         "use_proxy": True,
-        "host": "154.212.31.246",
-        "port": 62670,
-        "username": "hBeZwcdSM",
-        "password": "tXsEa8hci",
+        "host": "170.168.235.130",
+        "port": 64956,
+        "username": "3iNaX51Sa",
+        "password": "wZ7HwVwFe",
         "user_agent": "",
     }
 
